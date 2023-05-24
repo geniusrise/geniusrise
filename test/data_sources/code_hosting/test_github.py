14a4fb3acdee7213678152eb51cd4f7d7e13c719
ixaxaar <root@ixaxaar.in>
2023-05-24 18:02:03 +0530

    more github data and tests

--- test/data_sources/code_hosting/test_github.py
+++ test/data_sources/code_hosting/test_github.py
@@ -1,82 +1,43 @@
-from unittest.mock import MagicMock
-from github import Github
 from geniusrise_cli.data_sources.code_hosting.github import GithubDataFetcher
 
 
 def test_fetch_code():
-    # Mock the Github instance and repo
-    github_instance = MagicMock(spec=Github)
-    repo = MagicMock()
-    github_instance.get_repo.return_value = repo
-
-    # Mock the repo's get_contents method
-    file_content = MagicMock()
-    file_content.decoded_content = b"test.py"
-    repo.get_contents.return_value = [file_content]
-
     fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    fetcher.github = github_instance
     code_files = fetcher.fetch_code()
 
-    assert code_files[0][:5] == "<?php"
+    assert code_files[0][:10] == "File Name:"
 
 
 def test_fetch_pull_requests():
-    # Mock the Github instance and repo
-    github_instance = MagicMock(spec=Github)
-    repo = MagicMock()
-    github_instance.get_repo.return_value = repo
-
-    # Mock the repo's get_pulls method
-    pull_request = MagicMock()
-    pull_request.title = "Test Pull Request"
-    pull_request.body = "Test Pull Request Body"
-    pull_request.get_commits.return_value = []
-    pull_request.get_issue_comments.return_value = []
-    repo.get_pulls.return_value = [pull_request]
-
     fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    fetcher.github = github_instance
     pull_requests = fetcher.fetch_pull_requests()
 
-    assert pull_requests[0][:10] == "fixing min"
+    assert pull_requests[0][:10] == "Title: fix"
 
 
 def test_fetch_commits():
-    # Mock the Github instance and repo
-    github_instance = MagicMock(spec=Github)
-    repo = MagicMock()
-    github_instance.get_repo.return_value = repo
-
-    # Mock the repo's get_commits method
-    commit = MagicMock()
-    commit.commit.message = "Test Commit"
-    commit.files = []
-    repo.get_commits.return_value = [commit]
-
     fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    fetcher.github = github_instance
     commits = fetcher.fetch_commits()
 
-    assert commits[0][:10] == "Adding OPM"
+    assert commits[0][:10] == "Commit Mes"
 
 
 def test_fetch_issues():
-    # Mock the Github instance and repo
-    github_instance = MagicMock(spec=Github)
-    repo = MagicMock()
-    github_instance.get_repo.return_value = repo
-
-    # Mock the repo's get_issues method
-    issue = MagicMock()
-    issue.title = "Test Issue"
-    issue.body = "Test Issue Body"
-    issue.pull_request = None
-    issue.get_comments.return_value = []
-    repo.get_issues.return_value = [issue]
-
     fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    fetcher.github = github_instance
     issues = fetcher.fetch_issues()
 
-    assert issues[0][:10] == "fixing min"
+    assert issues[0][:10] == "Title: fix"
+
+
+def test_fetch_repo_details():
+    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
+    repo_details = fetcher.fetch_repo_details()
+
+    assert repo_details[0][:10] == "Repo Name:"
+
+
+def test_fetch_releases():
+    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
+    releases = fetcher.fetch_releases()
+
+    assert releases == []
