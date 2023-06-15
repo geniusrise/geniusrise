9ba1f7fee23f15d37c333ab8a9a1a878dc85c8b9
ixaxaar <root@ixaxaar.in>
2023-06-15 21:50:51 +0530

    github airflow task

--- geniusrise_cli/data_sources/code_hosting/github.py
+++ geniusrise_cli/data_sources/code_hosting/github.py
@@ -1,83 +1,66 @@
+<<<<<<< Updated upstream
 from github import Github
 from typing import List
 import os
 
+=======
+import os
+import json
+from typing import Any, Dict
+from github import Github
+>>>>>>> Stashed changes
 from geniusrise_cli.config import GITHUB_ACCESS_TOKEN
 from geniusrise_cli.data_sources.code_hosting.static import valid_extensions
+from task import Source
 
 
-class GithubDataFetcher:
-    def __init__(self, repo_name: str):
+class GithubDataFetcher(Source):
+    def __init__(self, repo_name: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.github = Github(GITHUB_ACCESS_TOKEN)
         self.repo = self.github.get_repo(repo_name)
 
-    def fetch_code(self) -> List[str]:
+    def fetch_code(self) -> str:
         contents = []
-        for file in self.repo.get_contents(""):  # type: ignore
-            if file.type == "file" and any(
-                file.name.endswith(ext) for ext in valid_extensions
-            ):  # fetch only files with valid extensions
-                contents.append(
-                    f"File Name: {file.name}{os.linesep}Content:{os.linesep}{file.decoded_content.decode()}"
-                )
-        return contents
+        for file in self.repo.get_contents(""):
+            if file.type == "file" and any(file.name.endswith(ext) for ext in valid_extensions):
+                contents.append(file.__dict__)
+        return self.save_to_file(contents, "code.json")
 
-    def fetch_pull_requests(self) -> List[str]:
+    def fetch_pull_requests(self) -> str:
         pull_requests = []
         for pr in self.repo.get_pulls(state="all"):
-            pr_data = [f"Title: {pr.title}", f"Body: {str(pr.body)}"]  # Convert pr.body to string
-            pr_data += [f"Commit Message: {commit.commit.message}" for commit in pr.get_commits()]
-            pr_data += [f"Comment: {comment.body}" for comment in pr.get_issue_comments()]
-            pull_requests.append(os.linesep.join(pr_data))
-        return pull_requests
+            pull_requests.append(pr.__dict__)
+        return self.save_to_file(pull_requests, "pull_requests.json")
 
-    def fetch_commits(self) -> List[str]:
+    def fetch_commits(self) -> str:
         commits = []
         for commit in self.repo.get_commits():
-            commit_data = [f"Commit Message: {commit.commit.message}"]
-            if commit.files:
-                commit_data += [f"File Patch: {file.patch}" for file in commit.files if file.patch]
-            commits.append(os.linesep.join(commit_data))
-        return commits
+            commits.append(commit.__dict__)
+        return self.save_to_file(commits, "commits.json")
 
-    def fetch_issues(self) -> List[str]:
+    def fetch_issues(self) -> str:
         issues = []
         for issue in self.repo.get_issues(state="all"):
-            issue_data = [f"Title: {issue.title}", f"Body: {str(issue.body)}"]  # Convert issue.body to string
-            issue_data += [f"Comment: {comment.body}" for comment in issue.get_comments()]
-            if issue.pull_request:
-                pr = self.repo.get_pull(issue.number)
-                issue_data += [f"PR Body: {str(pr.body)}"]  # Convert pr.body to string
-                issue_data += [f"Commit Message: {commit.commit.message}" for commit in pr.get_commits()]
-                issue_data += [f"Comment: {comment.body}" for comment in pr.get_issue_comments()]
-            issues.append(os.linesep.join(issue_data))
-        return issues
+            issues.append(issue.__dict__)
+        return self.save_to_file(issues, "issues.json")
 
-    def fetch_repo_details(self) -> List[str]:
-        contributors = self.repo.get_contributors()
-        contributor_data = [f"Handle: {contributor.login}, Name: {contributor.name}" for contributor in contributors]
-        readme_content = self.repo.get_readme().decoded_content.decode()
+    def fetch_repo_details(self) -> str:
+        contributors = [contributor.__dict__ for contributor in self.repo.get_contributors()]
+        readme_content = self.repo.get_readme().__dict__
         file_structure = self._get_file_structure(self.repo.get_contents(""))
-        repo_details = [
-            f"Repo Name: {self.repo.name}{os.linesep}"
-            f"Description: {self.repo.description}{os.linesep}"
-            f"Contributors: {os.linesep.join(contributor_data)}{os.linesep}"
-            f"Readme: {readme_content}{os.linesep}"
-            f"File Structure: {os.linesep.join(file_structure)}"
-        ]
-        return repo_details
+        repo_details = {
+            "name": self.repo.name,
+            "description": self.repo.description,
+            "contributors": contributors,
+            "readme": readme_content,
+            "file_structure": file_structure,
+        }
+        return self.save_to_file(repo_details, "repo_details.json")
 
-    def fetch_releases(self) -> List[str]:
-        releases = self.repo.get_releases()
-        release_data = [
-            f"Release Name: {release.title}{os.linesep}"
-            f"Release Notes: {release.body}{os.linesep}"
-            f"Released At: {release.published_at}{os.linesep}"
-            f"Version: {release.tag_name}{os.linesep}"
-            f"Other Metadata: {release.raw_data}"
-            for release in releases
-        ]
-        return release_data
+    def fetch_releases(self) -> str:
+        releases = [release.__dict__ for release in self.repo.get_releases()]
+        return self.save_to_file(releases, "releases.json")
 
     def _get_file_structure(self, contents) -> List[str]:
         structure = []
@@ -93,3 +76,17 @@ class GithubDataFetcher:
             else:
                 structure.append("File: " + content.name)
         return structure
+
+    def read(self, what_to_read: str) -> Any:
+        fetch_method = getattr(self, f"fetch_{what_to_read}", None)
+        if not fetch_method:
+            raise ValueError(f"Invalid read parameter: {what_to_read}")
+        local_folder = fetch_method()
+        self.sync_to_s3(local_folder)
+        return local_folder
+
+    def save_to_file(self, data: Dict[str, Any], filename: str) -> str:
+        local_dir = os.path.join(self.output_folder, filename)
+        with open(local_dir, "w") as f:
+            json.dump(data, f)
+        return local_dir
