8e63eb6abaa499523fa81b0dda7b9f09a239ad6b
ixaxaar <root@ixaxaar.in>
2023-06-16 21:46:02 +0530

    github redone

--- geniusrise_cli/data_sources/code_hosting/github.py
+++ geniusrise_cli/data_sources/code_hosting/github.py
@@ -1,68 +1,146 @@
-<<<<<<< Updated upstream
-from github import Github
-from typing import List
-import os
-
-=======
 import os
 import json
-from typing import Any, Dict
-from github import Github
->>>>>>> Stashed changes
+import requests
+import logging
+from typing import Any, List
+from github import Github, GithubException
+from github.ContentFile import ContentFile
 from geniusrise_cli.config import GITHUB_ACCESS_TOKEN
-from geniusrise_cli.data_sources.code_hosting.static import valid_extensions
-from task import Source
 
 
-class GithubDataFetcher(Source):
-    def __init__(self, repo_name: str, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class GithubDataFetcher:
+    def __init__(self, repo_name: str, output_folder: str):
+        """
+        Initialize GithubResourceFetcher with repository name, output folder, and access token.
+
+        :param repo_name: Name of the repository.
+        :param output_folder: Folder to save the fetched data.
+        :param access_token: Github access token.
+        """
         self.github = Github(GITHUB_ACCESS_TOKEN)
         self.repo = self.github.get_repo(repo_name)
+        self.output_folder = output_folder
 
-    def fetch_code(self) -> str:
-        contents = []
-        for file in self.repo.get_contents(""):
-            if file.type == "file" and any(file.name.endswith(ext) for ext in valid_extensions):
-                contents.append(file.__dict__)
-        return self.save_to_file(contents, "code.json")
+        self.log = logging.getLogger(__name__)
 
-    def fetch_pull_requests(self) -> str:
-        pull_requests = []
-        for pr in self.repo.get_pulls(state="all"):
-            pull_requests.append(pr.__dict__)
-        return self.save_to_file(pull_requests, "pull_requests.json")
+    def fetch_code(self):
+        """
+        Clone the repository to the output folder.
+        """
+        try:
+            os.system(f"git clone {self.repo.clone_url} {self.output_folder}")
+            self.log.info("Repository cloned successfully.")
+        except Exception as e:
+            self.log.error(f"Error cloning repository: {e}")
 
-    def fetch_commits(self) -> str:
-        commits = []
-        for commit in self.repo.get_commits():
-            commits.append(commit.__dict__)
-        return self.save_to_file(commits, "commits.json")
+    def fetch_pull_requests(self):
+        """
+        Fetch all pull requests and save each to a separate file.
+        """
+        try:
+            for pr in self.repo.get_pulls(state="all"):
+                diff_data = requests.get(pr.diff_url).text
+                patch_data = requests.get(pr.patch_url).text
+                pr_dict = {
+                    "number": pr.number,
+                    "title": pr.title,
+                    "body": pr.body,
+                    "comments": [comment.body for comment in pr.get_comments()],
+                    "diff": diff_data,
+                    "patch": patch_data,
+                }
+                self.save_to_file(pr_dict, f"pull_request_{pr.number}.json")
+            self.log.info("Pull requests fetched successfully.")
+        except GithubException as e:
+            self.log.error(f"Error fetching pull requests: {e}")
+        except requests.exceptions.RequestException as e:
+            self.log.error(f"Error fetching diff or patch data: {e}")
 
-    def fetch_issues(self) -> str:
-        issues = []
-        for issue in self.repo.get_issues(state="all"):
-            issues.append(issue.__dict__)
-        return self.save_to_file(issues, "issues.json")
+    def fetch_commits(self):
+        """
+        Fetch all commits and save each to a separate file.
+        """
+        try:
+            for commit in self.repo.get_commits():
+                diff_url = f"https://github.com/{self.repo.owner.login}/{self.repo.name}/commit/{commit.sha}.diff"
+                patch_url = f"https://github.com/{self.repo.owner.login}/{self.repo.name}/commit/{commit.sha}.patch"
+                diff_data = requests.get(diff_url).text
+                patch_data = requests.get(patch_url).text
+                commit_dict = {
+                    "sha": commit.sha,
+                    "message": commit.commit.message,
+                    "author": commit.commit.author.name,
+                    "date": commit.commit.author.date.isoformat(),
+                    "files_changed": [f.filename for f in commit.files],
+                    "diff": diff_data,
+                    "patch": patch_data,
+                }
+                self.save_to_file(commit_dict, f"commit_{commit.sha}.json")
+            self.log.info("Commits fetched successfully.")
+        except GithubException as e:
+            self.log.error(f"Error fetching commits: {e}")
+        except requests.exceptions.RequestException as e:
+            self.log.error(f"Error fetching diff or patch data: {e}")
 
-    def fetch_repo_details(self) -> str:
-        contributors = [contributor.__dict__ for contributor in self.repo.get_contributors()]
-        readme_content = self.repo.get_readme().__dict__
-        file_structure = self._get_file_structure(self.repo.get_contents(""))
-        repo_details = {
-            "name": self.repo.name,
-            "description": self.repo.description,
-            "contributors": contributors,
-            "readme": readme_content,
-            "file_structure": file_structure,
-        }
-        return self.save_to_file(repo_details, "repo_details.json")
+    def fetch_issues(self):
+        """
+        Fetch all issues and save each to a separate file.
+        """
+        try:
+            for issue in self.repo.get_issues(state="all"):
+                issue_dict = {
+                    "number": issue.number,
+                    "title": issue.title,
+                    "body": issue.body,
+                    "comments": [comment.body for comment in issue.get_comments()],
+                    "state": issue.state,
+                }
+                self.save_to_file(issue_dict, f"issue_{issue.number}.json")
+            self.log.info("Issues fetched successfully.")
+        except GithubException as e:
+            self.log.error(f"Error fetching issues: {e}")
 
-    def fetch_releases(self) -> str:
-        releases = [release.__dict__ for release in self.repo.get_releases()]
-        return self.save_to_file(releases, "releases.json")
+    def fetch_releases(self):
+        """
+        Fetch all releases and save each to a separate file.
+        """
+        try:
+            for release in self.repo.get_releases():
+                release_dict = {
+                    "tag_name": release.tag_name,
+                    "name": release.title,
+                    "body": release.body,
+                    "published_at": release.published_at.isoformat(),
+                }
+                self.save_to_file(release_dict, f"release_{release.tag_name}.json")
+            self.log.info("Releases fetched successfully.")
+        except GithubException as e:
+            self.log.error(f"Error fetching releases: {e}")
 
-    def _get_file_structure(self, contents) -> List[str]:
+    def fetch_repo_details(self):
+        """
+        Fetch repository details and save to a file.
+        """
+        try:
+            repo_details = {
+                "name": self.repo.name,
+                "description": self.repo.description,
+                "contributors": [contributor.login for contributor in self.repo.get_contributors()],
+                "readme": self.repo.get_readme().decoded_content.decode(),
+                "file_structure": self._get_file_structure(self.repo.get_contents("")),
+            }
+            self.save_to_file(repo_details, "repo_details.json")
+            self.log.info("Repository details fetched successfully.")
+        except GithubException as e:
+            self.log.error(f"Error fetching repository details: {e}")
+
+    def _get_file_structure(self, contents: List[ContentFile]) -> List[str]:
+        """
+        Get the file structure of the repository.
+
+        :param contents: List of repository contents.
+        :return: List of file and directory names.
+        """
         structure = []
         for content in contents:
             if content.type == "dir":
@@ -71,22 +149,41 @@ class GithubDataFetcher(Source):
                     + content.name
                     + "\n"
                     + "Contents: \n"
-                    + "\n".join(self._get_file_structure(self.repo.get_contents(content.path)))
+                    + "\n".join(self._get_file_structure(self.repo.get_contents(content.path)))  # type: ignore
                 )
             else:
                 structure.append("File: " + content.name)
         return structure
 
-    def read(self, what_to_read: str) -> Any:
-        fetch_method = getattr(self, f"fetch_{what_to_read}", None)
-        if not fetch_method:
-            raise ValueError(f"Invalid read parameter: {what_to_read}")
-        local_folder = fetch_method()
-        self.sync_to_s3(local_folder)
-        return local_folder
+    def save_to_file(self, data: Any, filename: str):
+        """
+        Save data to a file in the output folder.
 
-    def save_to_file(self, data: Dict[str, Any], filename: str) -> str:
-        local_dir = os.path.join(self.output_folder, filename)
-        with open(local_dir, "w") as f:
-            json.dump(data, f)
-        return local_dir
+        :param data: Data to save.
+        :param filename: Name of the file to save the data.
+        """
+        try:
+            local_dir = os.path.join(self.output_folder, filename)
+            with open(local_dir, "w") as f:
+                json.dump(data, f)
+            self.log.info(f"Data saved to {filename}.")
+        except Exception as e:
+            self.log.error(f"Error saving data to file: {e}")
+
+    def get(self, resource_type: str) -> str:
+        """
+        Call the appropriate function based on the resource type, save the data, and return the status.
+
+        :param resource_type: Type of the resource to fetch.
+        :return: Status message.
+        """
+        fetch_method = getattr(self, f"fetch_{resource_type}", None)
+        if not fetch_method:
+            self.log.error(f"Invalid resource type: {resource_type}")
+            return f"Invalid resource type: {resource_type}"
+        try:
+            fetch_method()
+            return f"{resource_type} fetched successfully."
+        except Exception as e:
+            self.log.error(f"Error fetching {resource_type}: {e}")
+            return f"Error fetching {resource_type}: {e}"
--- test/data_sources/code_hosting/test_github.py
+++ test/data_sources/code_hosting/test_github.py
@@ -1,43 +1,66 @@
+import json
+import tempfile
+import os
 from geniusrise_cli.data_sources.code_hosting.github import GithubDataFetcher
 
-
-def test_fetch_code():
-    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    code_files = fetcher.fetch_code()
-
-    assert code_files[0][:10] == "File Name:"
+# Initialize GithubResourceFetcher with the public repository "requests/requests"
+output_folder = tempfile.mkdtemp()
+fetcher = GithubDataFetcher("zpqrtbnk/test-repo", output_folder)
 
 
 def test_fetch_pull_requests():
-    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    pull_requests = fetcher.fetch_pull_requests()
-
-    assert pull_requests[0][:10] == "Title: fix"
+    fetcher.fetch_pull_requests()
+    # Check that the 5th pull request contains the word "fix"
+    with open(f"{output_folder}/pull_request_123.json") as f:
+        pr_data = json.load(f)
+    assert "Add text document with hello message" in pr_data["body"]
 
 
 def test_fetch_commits():
-    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    commits = fetcher.fetch_commits()
-
-    assert commits[0][:10] == "Commit Mes"
+    fetcher.fetch_commits()
+    # Check that the 10th commit message contains the word "update"
+    with open(f"{output_folder}/commit_21c2a100246d498732557c67302bad1dd3c3c8d0.json") as f:
+        commit_data = json.load(f)
+    assert "wflow" in commit_data["commit"]["message"]
 
 
 def test_fetch_issues():
-    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    issues = fetcher.fetch_issues()
-
-    assert issues[0][:10] == "Title: fix"
-
-
-def test_fetch_repo_details():
-    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    repo_details = fetcher.fetch_repo_details()
-
-    assert repo_details[0][:10] == "Repo Name:"
+    fetcher.fetch_issues()
+    # Check that the 3rd issue title contains the word "error"
+    with open(f"{output_folder}/issue_104.json") as f:
+        issue_data = json.load(f)
+    assert "update readme" in issue_data["title"]
 
 
 def test_fetch_releases():
-    fetcher = GithubDataFetcher(repo_name="rmccue/test-repository")
-    releases = fetcher.fetch_releases()
+    fetcher.fetch_releases()
+    # Check that the 1st release tag name is "v2.26.0"
+    with open(f"{output_folder}/release_v4.5.6.json") as f:
+        release_data = json.load(f)
+    assert release_data["tag_name"] == "v4.5.6"
 
-    assert releases == []
+
+def test_fetch_repo_details():
+    fetcher.fetch_repo_details()
+    # Check that the repository name is "requests"
+    with open(f"{output_folder}/repo_details.json") as f:
+        repo_data = json.load(f)
+    assert repo_data["name"] == "test-repo"
+
+
+def test_fetch_code():
+    fetcher.fetch_code()
+    # Check that the repository was cloned by checking if the .git directory exists
+    assert os.path.isdir(f"{output_folder}/.git")
+
+
+def test_get_positive():
+    # Test the get method with a valid resource type
+    status = fetcher.get("issues")
+    assert status == "issues fetched successfully."
+
+
+def test_get_negative():
+    # Test the get method with an invalid resource type
+    status = fetcher.get("invalid_resource_type")
+    assert status == "Invalid resource type: invalid_resource_type"
