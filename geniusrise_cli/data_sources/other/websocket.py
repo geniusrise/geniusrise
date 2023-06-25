fff62d5f77c3d4c7566ae2d4232ffcd6ca47003b
ixaxaar <root@ixaxaar.in>
2023-06-26 00:37:15 +0530

    add websockets and HTTP polling

---
+++ geniusrise_cli/data_sources/other/websocket.py
@@ -0,0 +1,35 @@
+import asyncio
+import websockets
+from geniusrise_cli.data_sources.streaming import StreamingDataFetcher
+
+
+class WebSocketDataFetcher(StreamingDataFetcher):
+    def __init__(self, handler=None, state_manager=None, host: str = "localhost", port: int = 8765):
+        super().__init__(handler, state_manager)
+        self.host = host
+        self.port = port
+
+    async def __listen(self):
+        """
+        Start listening for data from the WebSocket server.
+        """
+        async with websockets.serve(self.receive_message, self.host, self.port):
+            await asyncio.Future()  # run forever
+
+    async def receive_message(self, websocket, path):
+        """
+        Receive a message from a WebSocket client and save it.
+
+        :param websocket: WebSocket client connection.
+        :param path: WebSocket path.
+        """
+        data = await websocket.recv()
+        self.save(data, "websocket.json")
+        self.update_state("success")
+
+    def listen(self):
+        """
+        Start the WebSocket server.
+        """
+        self.__listen()
+        asyncio.run(self.__listen())
