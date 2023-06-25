6b87edd078f252836fb276c283873b55c4ceb127
ixaxaar <russi@setu.co>
2023-06-25 19:32:22 +0530

    add discord and slack

---
+++ geniusrise_cli/data_sources/communication/slack.py
@@ -0,0 +1,70 @@
+from slack_sdk import WebClient
+from slack_sdk.socket_mode import SocketModeClient
+from slack_sdk.socket_mode.request import SocketModeRequest
+from slack_sdk.socket_mode.response import SocketModeResponse
+from geniusrise_cli.data_sources.batch import BatchDataFetcher
+from geniusrise_cli.data_sources.stream import StreamingDataFetcher
+
+
+class SlackBatchDataFetcher(BatchDataFetcher):
+    def __init__(self, token: str, handler=None, state_manager=None):
+        super().__init__(handler, state_manager)
+        self.client = WebClient(token=token)
+
+    def fetch_all(self):
+        """
+        Fetch all historical data from all channels in the Slack workspace.
+        """
+        try:
+            channels = self.client.conversations_list()["channels"]
+            for channel in channels:
+                self._fetch_channel(channel["id"])
+            self.update_state("success")
+        except Exception as e:
+            self.log.error(f"Error fetching all channels: {e}")
+            self.update_state("failure")
+
+    def _fetch_channel(self, channel_id: str):
+        """
+        Fetch all historical data from a specific channel.
+
+        :param channel_id: ID of the channel.
+        """
+        try:
+            cursor = None
+            while True:
+                response = self.client.conversations_history(channel=channel_id, cursor=cursor)
+                messages = response["messages"]
+                self.save(messages, f"{channel_id}.json")
+                cursor = response["response_metadata"]["next_cursor"]
+                if not cursor:
+                    break
+        except Exception as e:
+            self.log.error(f"Error fetching channel {channel_id}: {e}")
+
+
+class SlackStreamingDataFetcher(StreamingDataFetcher):
+    def __init__(self, token: str, app_token: str, handler=None, state_manager=None):
+        super().__init__(handler, state_manager)
+        self.client = WebClient(token=token)
+        self.socket_mode_client = SocketModeClient(app_token=app_token)
+
+    def listen(self):
+        """
+        Start the Socket Mode client and listen for new messages in all channels.
+        """
+
+        @self.socket_mode_client.socket_mode_request_listeners.add
+        def handle_socket_mode_request(req: SocketModeRequest):
+            if req.type == "events_api":
+                event = req.payload["event"]
+                if event["type"] == "message":
+                    try:
+                        self.save(event, f"{event['channel']}.json")
+                        self.update_state("success")
+                    except Exception as e:
+                        self.log.error(f"Error saving message from channel {event['channel']}: {e}")
+                        self.update_state("failure")
+            return SocketModeResponse(envelope_id=req.envelope_id)
+
+        self.socket_mode_client.connect()
--- geniusrise_cli/data_sources/other/webhook.py
+++ geniusrise_cli/data_sources/other/webhook.py
@@ -1,23 +1,24 @@
 from flask import Flask, request
-from geniusrise_cli.data_sources.stream import StreamingDataFetcher
+from geniusrise_cli.data_sources.streaming import StreamingDataFetcher
 import threading
 
 
-class WhatsAppDataFetcher(StreamingDataFetcher):
-    def __init__(self, handler=None, state_manager=None, port: int = 3000):
+class WebhookDataFetcher(StreamingDataFetcher):
+    def __init__(self, endpoint: str, handler=None, state_manager=None, port: int = 3000):
         super().__init__(handler, state_manager)
         self.app = Flask(__name__)
+        self.endpoint = endpoint
         self.port = port
 
-        @self.app.route("/whatsapp", methods=["POST"])
+        @self.app.route(self.endpoint, methods=["GET", "POST", "PUT", "DELETE"])
         def handle_webhook():
             data = request.get_json()
-            self.save(data, "whatsapp.json")
+            self.save(data, "webhook.json")
             self.update_state("success")
             return "", 200
 
     def listen(self):
         """
-        Start listening for data from the WhatsApp webhook.
+        Start listening for data from the webhook.
         """
         threading.Thread(target=self.app.run, kwargs={"host": "0.0.0.0", "port": self.port}).start()
