07ee5b3db461c34d067908d1804327d7aab4e793
ixaxaar <russi@setu.co>
2023-06-28 00:47:00 +0530

    add zerodha and aa

---
+++ geniusrise_cli/data_sources/finance/account_aggregator.py
@@ -0,0 +1,35 @@
+import requests
+from geniusrise_cli.data_sources.streaming_data_fetcher import StreamingDataFetcher
+
+
+class FIUNotificationFetcher(StreamingDataFetcher):
+    """
+    A streaming data fetcher for the /FI/Notification endpoint.
+    """
+
+    def __init__(self, handler=None, state_manager=None):
+        super().__init__(handler, state_manager)
+        self.endpoint = "https://your-api-url.com/FI/Notification"  # replace with your actual endpoint
+
+    def listen(self):
+        """
+        Start listening for data from the /FI/Notification endpoint.
+        """
+        while True:
+            try:
+                response = requests.get(self.endpoint)
+                response.raise_for_status()
+                data = response.json()
+                self.save(data, "fi_notification.json")
+                self.update_state("success")
+            except Exception as e:
+                self.log.error(f"Error fetching data: {e}")
+                self.update_state("failure")
+
+    def __repr__(self) -> str:
+        """
+        Return a string representation of the streaming data fetcher.
+
+        :return: A string representation of the streaming data fetcher.
+        """
+        return f"FI Notification fetcher: {self.__class__.__name__}"
