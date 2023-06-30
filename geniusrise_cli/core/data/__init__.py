12033e114aa1460c34641875e51be930c7647d21
ixaxaar <russi@setu.co>
2023-06-30 14:00:11 +0530

    data handling

--- geniusrise_cli/core/data/__init__.py
+++ geniusrise_cli/core/data/__init__.py
@@ -2,3 +2,5 @@ from .batch_output import BatchOutputConfig
 from .input import InputConfig
 from .output import OutputConfig
 from .streaming_output import StreamingOutputConfig
+from .batch_input import BatchInputConfig
+from .streaming_input import StreamingInputConfig
