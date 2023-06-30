31ae29ddbd81a9d0c41d2a98ea3b16a8c1cd46ee
ixaxaar <russi@setu.co>
2023-06-30 19:49:10 +0530

    add bolt and spout factories

---
+++ geniusrise_cli/core/__init__.py
@@ -0,0 +1,2 @@
+from .bolt import BoltFactory
+from .spout import SpoutFactory
---
+++ geniusrise_cli/core/spout.py
@@ -0,0 +1,71 @@
+from .input_config import BatchInputConfig, StreamingInputConfig
+from .output_config import BatchOutputConfig, StreamingOutputConfig
+from .state_manager import (
+    StateManager,
+    InMemoryStateManager,
+    RedisStateManager,
+    PostgresStateManager,
+    DynamoDBStateManager,
+)
+from .task import Task, ECSTask, K8sTask
+
+
+class SpoutFactory:
+    """
+    Factory class for creating spouts of various types.
+    """
+
+    @staticmethod
+    def create(input_type: str, output_type: str, state_type: str, task_type: str, **kwargs) -> Task:
+        """
+        Create a spout of a specific type.
+
+        Args:
+            input_type (str): The type of input config ("batch" or "streaming").
+            output_type (str): The type of output config ("batch" or "streaming").
+            state_type (str): The type of state manager ("in_memory", "redis", "postgres", or "dynamodb").
+            task_type (str): The type of task ("ecs" or "k8s").
+            **kwargs: Additional keyword arguments for initializing the spout.
+
+        Returns:
+            Task: The created spout.
+        """
+        # Create the input config
+        if input_type == "batch":
+            input_config = BatchInputConfig(**kwargs)
+        elif input_type == "streaming":
+            input_config = StreamingInputConfig(**kwargs)
+        else:
+            raise ValueError(f"Invalid input type: {input_type}")
+
+        # Create the output config
+        if output_type == "batch":
+            output_config = BatchOutputConfig(**kwargs)
+        elif output_type == "streaming":
+            output_config = StreamingOutputConfig(**kwargs)
+        else:
+            raise ValueError(f"Invalid output type: {output_type}")
+
+        # Create the state manager
+        state_manager: StateManager
+        if state_type == "in_memory":
+            state_manager = InMemoryStateManager()
+        elif state_type == "redis":
+            state_manager = RedisStateManager(**kwargs)
+        elif state_type == "postgres":
+            state_manager = PostgresStateManager(**kwargs)
+        elif state_type == "dynamodb":
+            state_manager = DynamoDBStateManager(**kwargs)
+        else:
+            raise ValueError(f"Invalid state type: {state_type}")
+
+        # Create the task
+        task: Task
+        if task_type == "ecs":
+            task = ECSTask(input_config, output_config, state_manager, **kwargs)
+        elif task_type == "k8s":
+            task = K8sTask(input_config, output_config, state_manager, **kwargs)
+        else:
+            raise ValueError(f"Invalid task type: {task_type}")
+
+        return task
