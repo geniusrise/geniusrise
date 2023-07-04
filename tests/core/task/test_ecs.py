962069af2cc20f8618f9ce59770a0feb34e0d41c
ixaxaar <root@ixaxaar.in>
2023-07-05 01:52:51 +0530

    tests for ecs

--- tests/core/task/test_ecs.py
+++ tests/core/task/test_ecs.py
@@ -1,75 +1,74 @@
 import pytest
 from geniusrise.core.task import ECSManager
 
-# Define your ECSManager details as constants
+# Hardcode your AWS resources here
+ACCPUNT_ID = "866011655254"
 CLUSTER = "test-cluster"
-NAME = "test-task"
-COMMAND = ["echo", "hello"]
-IMAGE = "busybox"
-REPLICAS = 1
-PORT = 80
-LOG_GROUP = "/ecs/geniusrise-test-task"
 SUBNET_IDS = ["subnet-28ce4853", "subnet-99700cd5"]
+SECURITY_GROUP_IDS = ["sg-0e236b30891c3ed6d"]
+TASK_NAME = "test-task"
+TASK_COMMAND = ["echo", "hello"]
+TASK_IMAGE = "alpine"
+TASK_PORT = 8080
 
 
-# Define a fixture for your ECSManager
-@pytest.fixture
+@pytest.fixture(scope="module")
 def ecs_manager():
     return ECSManager(
-        name=NAME,
-        command=COMMAND,
-        image=IMAGE,
-        replicas=REPLICAS,
-        port=PORT,
-        log_group=LOG_GROUP,
+        name=TASK_NAME,
+        command=TASK_COMMAND,
         cluster=CLUSTER,
         subnet_ids=SUBNET_IDS,
+        security_group_ids=SECURITY_GROUP_IDS,
+        image=TASK_IMAGE,
+        port=TASK_PORT,
+        account_id=ACCPUNT_ID,
     )
 
 
-# Test that the ECSManager can be initialized
-def test_ecs_manager_init(ecs_manager):
-    assert ecs_manager.name == NAME
-    assert ecs_manager.command == COMMAND
-    assert ecs_manager.image == IMAGE
-    assert ecs_manager.replicas == REPLICAS
-    assert ecs_manager.port == PORT
-    assert ecs_manager.log_group == LOG_GROUP
-
-
-# Test that the ECSManager can create a task definition
-def test_ecs_manager_create_task_definition(ecs_manager):
+def test_create_task_definition(ecs_manager):
     task_definition_arn = ecs_manager.create_task_definition()
-    assert task_definition_arn is not None
+    assert "arn:aws:ecs:ap-south-1:866011655254:task-definition/test-task:" in task_definition_arn
 
 
-# # Test that the ECSManager can run a task
-# def test_ecs_manager_run_task(ecs_manager):
+def test_run_task(ecs_manager):
+    task_definition_arn = ecs_manager.create_task_definition()
+    response = ecs_manager.run_task(task_definition_arn)
+    assert response["failures"] == []
+    assert response["tasks"][0]["cpu"] == "256"
+    assert response["tasks"][0]["launchType"] == "FARGATE"
+
+
+def test_describe_task(ecs_manager):
+    task_definition_arn = ecs_manager.create_task_definition()
+    task = ecs_manager.run_task(task_definition_arn)
+    response = ecs_manager.describe_task(task["tasks"][0]["taskArn"])
+    assert response is not None
+
+
+def test_stop_task(ecs_manager):
+    task_definition_arn = ecs_manager.create_task_definition()
+    task = ecs_manager.run_task(task_definition_arn)
+    response = ecs_manager.stop_task(task["tasks"][0]["taskArn"])
+    assert response is not None
+
+
+# def test_create_service(ecs_manager):
 #     task_definition_arn = ecs_manager.create_task_definition()
-#     response = ecs_manager.run_task(task_definition_arn)
+#     response = ecs_manager.create_service(task_definition_arn)
 #     assert response is not None
 
 
-# # Test that the ECSManager can describe a task
-# def test_ecs_manager_describe_task(ecs_manager):
+# def test_update_service(ecs_manager):
 #     task_definition_arn = ecs_manager.create_task_definition()
-#     ecs_manager.run_task(task_definition_arn)
-#     response = ecs_manager.describe_task(task_definition_arn)
+#     ecs_manager.create_service(task_definition_arn)
+#     new_task_definition_arn = ecs_manager.create_task_definition()
+#     response = ecs_manager.update_service(new_task_definition_arn)
 #     assert response is not None
 
 
-# # Test that the ECSManager can stop a task
-# def test_ecs_manager_stop_task(ecs_manager):
+# def test_delete_service(ecs_manager):
 #     task_definition_arn = ecs_manager.create_task_definition()
-#     ecs_manager.run_task(task_definition_arn)
-#     response = ecs_manager.stop_task(task_definition_arn)
+#     ecs_manager.create_service(task_definition_arn)
+#     response = ecs_manager.delete_service()
 #     assert response is not None
-
-
-# # Test that the ECSManager can update a task
-# def test_ecs_manager_update_task(ecs_manager):
-#     new_image = "alpine"
-#     new_command = ["echo", "world"]
-#     ecs_manager.update_task(new_image, new_command)
-#     assert ecs_manager.image == new_image
-#     assert ecs_manager.command == new_command
