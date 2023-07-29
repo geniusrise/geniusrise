110751aed9a5901488803109c2f7b967e1596cec
ixaxaar <root@ixaxaar.in>
2023-07-30 02:51:58 +0530

    add typing

--- geniusrise/bolts/huggingface/commonsense_reasoning.py
+++ geniusrise/bolts/huggingface/commonsense_reasoning.py
@@ -1,3 +1,4 @@
+from typing import Dict, Any
 from datasets import load_from_disk
 from transformers import DataCollatorWithPadding
 
@@ -18,7 +19,7 @@ class CommonsenseReasoningFineTuner(HuggingFaceBatchFineTuner):
         - 'label': an integer representing the label (0 for entailment, 1 for neutral, 2 for contradiction).
     """
 
-    def load_dataset(self, dataset_path, **kwargs):
+    def load_dataset(self, dataset_path: str, **kwargs: Any) -> Dict:
         """
         Load a dataset from a directory.
 
@@ -29,15 +30,21 @@ class CommonsenseReasoningFineTuner(HuggingFaceBatchFineTuner):
         Returns:
             Dataset: The loaded dataset.
         """
-        # Load the dataset from the directory
-        dataset = load_from_disk(dataset_path)
+        try:
+            # Load the dataset from the directory
+            dataset = load_from_disk(dataset_path)
 
-        # Preprocess the dataset
-        tokenized_dataset = dataset.map(self.prepare_train_features, batched=True, remove_columns=dataset.column_names)
+            # Preprocess the dataset
+            tokenized_dataset = dataset.map(
+                self.prepare_train_features, batched=True, remove_columns=dataset.column_names
+            )
 
-        return tokenized_dataset
+            return tokenized_dataset
+        except Exception as e:
+            print(f"Error loading dataset: {e}")
+            raise
 
-    def prepare_train_features(self, examples):
+    def prepare_train_features(self, examples: Dict) -> Dict:
         """
         Tokenize the examples and prepare the features for training.
 
@@ -47,15 +54,21 @@ class CommonsenseReasoningFineTuner(HuggingFaceBatchFineTuner):
         Returns:
             dict: The processed features.
         """
-        # Tokenize the examples
-        tokenized_inputs = self.tokenizer(examples["premise"], examples["hypothesis"], truncation=True, padding=False)
+        try:
+            # Tokenize the examples
+            tokenized_inputs = self.tokenizer(
+                examples["premise"], examples["hypothesis"], truncation=True, padding=False
+            )
 
-        # Prepare the labels
-        tokenized_inputs["labels"] = examples["label"]
+            # Prepare the labels
+            tokenized_inputs["labels"] = examples["label"]
 
-        return tokenized_inputs
+            return tokenized_inputs
+        except Exception as e:
+            print(f"Error preparing train features: {e}")
+            raise
 
-    def data_collator(self, examples):
+    def data_collator(self, examples: Dict) -> Dict:
         """
         Customize the data collator.
 
@@ -65,4 +78,8 @@ class CommonsenseReasoningFineTuner(HuggingFaceBatchFineTuner):
         Returns:
             dict: The collated data.
         """
-        return DataCollatorWithPadding(self.tokenizer)(examples)
+        try:
+            return DataCollatorWithPadding(self.tokenizer)(examples)
+        except Exception as e:
+            print(f"Error in data collation: {e}")
+            raise
