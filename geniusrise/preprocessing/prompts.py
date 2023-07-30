709ad205cff066878905899e045534c81e2e7e80
ixaxaar <root@ixaxaar.in>
2023-07-31 01:40:36 +0530

    add license header

--- geniusrise/preprocessing/prompts.py
+++ geniusrise/preprocessing/prompts.py
@@ -1,3 +1,20 @@
+# geniusrise
+# Copyright (C) 2023  geniusrise.ai
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 def prompt_generate_prompts(x: str):
     return f"""
 generate 10 prompts to generate this {x} (that i intend to use to fine tune a model to be able to generate this {x} when prompted by those prompts):
