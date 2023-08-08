cc200f4ff2989e1dd0f4f76249dd3dc3521a3cc3
ixaxaar <root@ixaxaar.in>
2023-08-09 01:05:50 +0530

    update yaml sructure and add tests

--- geniusrise/cli/schema.py
+++ geniusrise/cli/schema.py
@@ -19,12 +19,21 @@ from typing import Dict, List, Optional
 from pydantic import BaseModel, Extra, validator
 
 
-class SpoutKwargs(BaseModel):
+class ExtraKwargs(BaseModel):
+    """
+    This class is used to handle any extra arguments that are not explicitly defined in the schema.
+    """
+
     class Config:
         extra = Extra.allow
 
 
 class StateArgs(BaseModel):
+    """
+    This class defines the arguments for the state. Depending on the type of state (in_memory, redis, postgres, dynamodb),
+    different arguments are required.
+    """
+
     redis_host: Optional[str]
     redis_port: Optional[int]
     redis_db: Optional[int]
@@ -42,6 +51,10 @@ class StateArgs(BaseModel):
 
 
 class State(BaseModel):
+    """
+    This class defines the state of the spout or bolt. The state can be of type in_memory, redis, postgres, or dynamodb.
+    """
+
     type: str
     args: Optional[StateArgs]
 
@@ -51,7 +64,7 @@ class State(BaseModel):
             raise ValueError("Invalid state type")
         return v
 
-    @validator("args", pre=True)
+    @validator("args", pre=True, always=True)
     def validate_args(cls, v, values, **kwargs):
         if "type" in values:
             if values["type"] == "redis":
@@ -75,9 +88,13 @@ class State(BaseModel):
 
 
 class OutputArgs(BaseModel):
-    output_folder: Optional[str]
+    """
+    This class defines the arguments for the output. Depending on the type of output (batch, streaming),
+    different arguments are required.
+    """
+
     bucket: Optional[str]
-    s3_folder: Optional[str]
+    folder: Optional[str]
     output_topic: Optional[str]
     kafka_servers: Optional[str]
 
@@ -86,6 +103,10 @@ class OutputArgs(BaseModel):
 
 
 class Output(BaseModel):
+    """
+    This class defines the output of the spout or bolt. The output can be of type batch or streaming.
+    """
+
     type: str
     args: Optional[OutputArgs]
 
@@ -95,11 +116,11 @@ class Output(BaseModel):
             raise ValueError("Invalid output type")
         return v
 
-    @validator("args", pre=True)
+    @validator("args", pre=True, always=True)
     def validate_args(cls, v, values, **kwargs):
         if "type" in values:
             if values["type"] == "batch":
-                if not v or "output_folder" not in v or "bucket" not in v or "s3_folder" not in v:
+                if not v or "bucket" not in v or "folder" not in v:
                     raise ValueError("Missing required fields for batch output type")
             elif values["type"] == "streaming":
                 if not v or "output_topic" not in v or "kafka_servers" not in v:
@@ -107,7 +128,54 @@ class Output(BaseModel):
         return v
 
 
+class InputArgs(BaseModel):
+    """
+    This class defines the arguments for the input. Depending on the type of input (batch, streaming),
+    different arguments are required.
+    """
+
+    input_topic: Optional[str]
+    kafka_servers: Optional[str]
+    output_folder: Optional[str]
+    bucket: Optional[str]
+    folder: Optional[str]
+
+    class Config:
+        extra = Extra.allow
+
+
+class Input(BaseModel):
+    """
+    This class defines the input of the bolt. The input can be of type batch or streaming.
+    """
+
+    type: str
+    args: Optional[InputArgs]
+
+    @validator("type")
+    def validate_type(cls, v, values, **kwargs):
+        if v not in ["batch", "streaming"]:
+            raise ValueError("Invalid input type")
+        return v
+
+    @validator("args", pre=True, always=True)
+    def validate_args(cls, v, values, **kwargs):
+        if "type" in values:
+            if values["type"] == "batch":
+                if not v or "bucket" not in v or "folder" not in v:
+                    raise ValueError("Missing required fields for batch input type")
+            elif values["type"] == "streaming":
+                if not v or "input_topic" not in v or "kafka_servers" not in v:
+                    raise ValueError("Missing required fields for streaming input type")
+        return v
+
+
 class DeployArgs(BaseModel):
+    """
+    This class defines the arguments for the deployment. Depending on the type of deployment (k8s, ecs),
+    different arguments are required.
+    """
+
     name: Optional[str]
     namespace: Optional[str]
     image: Optional[str]
@@ -125,6 +193,10 @@ class DeployArgs(BaseModel):
 
 
 class Deploy(BaseModel):
+    """
+    This class defines the deployment of the spout or bolt. The deployment can be of type k8s or ecs.
+    """
+
     type: str
     args: Optional[DeployArgs]
 
@@ -134,19 +206,69 @@ class Deploy(BaseModel):
             raise ValueError("Invalid deploy type")
         return v
 
+    @validator("args", pre=True, always=True)
+    def validate_args(cls, v, values, **kwargs):
+        if "type" in values:
+            if values["type"] == "ecs":
+                required_fields = [
+                    "name",
+                    "namespace",
+                    "image",
+                    "replicas",
+                    "account_id",
+                    "cluster",
+                    "subnet_ids",
+                    "security_group_ids",
+                    "log_group",
+                    "cpu",
+                    "memory",
+                ]
+                for field in required_fields:
+                    if not v or field not in v or not v[field]:
+                        raise ValueError(f"Missing required field '{field}' for ecs deploy type")
+            elif values["type"] == "k8s":
+                required_fields = ["name", "namespace", "image", "replicas"]
+                for field in required_fields:
+                    if not v or field not in v or not v[field]:
+                        raise ValueError(f"Missing required field '{field}' for k8s deploy type")
+        return v
+
 
 class Spout(BaseModel):
+    """
+    This class defines a spout. A spout has a name, method, optional arguments, output, state, and deployment.
+    """
+
     name: str
     method: str
+    args: Optional[ExtraKwargs]
     output: Output
     state: State
-    other: SpoutKwargs
     deploy: Deploy
 
 
-class SpoutConfig(BaseModel):
+class Bolt(BaseModel):
+    """
+    This class defines a bolt. A bolt has a name, method, optional arguments, input, output, state, and deployment.
+    """
+
+    name: str
+    method: str
+    args: Optional[ExtraKwargs]
+    input: Input
+    output: Output
+    state: State
+    deploy: Deploy
+
+
+class Geniusfile(BaseModel):
+    """
+    This class defines the overall structure of the YAML file. It includes a version, spouts, and bolts.
+    """
+
     version: str
     spouts: Dict[str, Spout]
+    bolts: Dict[str, Bolt]
 
     @validator("version")
     def validate_version(cls, v, values, **kwargs):
---
+++ geniusrise/cli/tests/test_schema.py
@@ -0,0 +1,148 @@
+import pytest
+from pydantic import ValidationError
+from geniusrise.cli.schema import Geniusfile
+
+# Correct YAML data for testing
+correct_yaml_data = {
+    "version": "1",
+    "spouts": {
+        "github-batch": {
+            "name": "GithubDump",
+            "method": "fetch_pull_requests",
+            "args": {"repo_name": "zpqrtbnk/test-repo", "github_access_token": "test"},
+            "output": {"type": "batch", "args": {"bucket": "my-bucket", "folder": "my-s3-folder"}},
+            "state": {"type": "in_memory"},
+            "deploy": {
+                "type": "k8s",
+                "args": {
+                    "name": "github-dump",
+                    "namespace": "geniusrise",
+                    "image": "geniusrise/geniusrise",
+                    "replicas": 1,
+                },
+            },
+        }
+    },
+    "bolts": {},
+}
+
+# Incorrect YAML data for testing
+incorrect_yaml_data = {
+    "version": "1",
+    "spouts": {
+        "github-batch": {
+            "name": "GithubDump",
+            "method": "fetch_pull_requests",
+            "args": {"repo_name": "zpqrtbnk/test-repo"},
+            "output": {"type": "batch", "args": {"bucket": "my-bucket"}},
+            "state": {"type": "in_memory"},
+            "deploy": {
+                "type": "k8s",
+                "args": {"name": "github-dump", "namespace": "geniusrise", "image": "geniusrise/geniusrise"},
+            },
+        }
+    },
+    "bolts": {},
+}
+
+# Data with missing required fields
+missing_fields_data = {
+    "version": "1",
+    "spouts": {
+        "github-batch": {
+            "name": "GithubDump",
+            "method": "fetch_pull_requests",
+            "output": {"type": "batch", "args": {"bucket": "my-bucket", "folder": "my-s3-folder"}},
+            "state": {"type": "in_memory"},
+        }
+    },
+    "bolts": {},
+}
+
+# Data with invalid field values
+invalid_values_data = {
+    "version": "1",
+    "spouts": {
+        "github-batch": {
+            "name": "GithubDump",
+            "method": "fetch_pull_requests",
+            "args": {"repo_name": "zpqrtbnk/test-repo", "github_access_token": "test"},
+            "output": {"type": "invalid_type", "args": {"bucket": "my-bucket", "folder": "my-s3-folder"}},
+            "state": {"type": "in_memory"},
+            "deploy": {
+                "type": "k8s",
+                "args": {
+                    "name": "github-dump",
+                    "namespace": "geniusrise",
+                    "image": "geniusrise/geniusrise",
+                    "replicas": 1,
+                },
+            },
+        }
+    },
+    "bolts": {},
+}
+
+# Data with extra fields
+extra_fields_data = {
+    "version": "1",
+    "spouts": {
+        "github-batch": {
+            "name": "GithubDump",
+            "method": "fetch_pull_requests",
+            "args": {"repo_name": "zpqrtbnk/test-repo", "github_access_token": "test", "extra_field": "extra_value"},
+            "output": {"type": "batch", "args": {"bucket": "my-bucket", "folder": "my-s3-folder"}},
+            "state": {"type": "in_memory"},
+            "deploy": {
+                "type": "k8s",
+                "args": {
+                    "name": "github-dump",
+                    "namespace": "geniusrise",
+                    "image": "geniusrise/geniusrise",
+                    "replicas": 1,
+                },
+            },
+        }
+    },
+    "bolts": {},
+}
+
+
+def test_correct_yaml_validation():
+    """Test that the correct YAML data is validated without errors."""
+    geniusfile = Geniusfile(**correct_yaml_data)
+    assert geniusfile.version == "1"
+    assert "github-batch" in geniusfile.spouts
+
+
+def test_incorrect_yaml_validation():
+    """Test that the incorrect YAML data raises a validation error."""
+    with pytest.raises(ValidationError):
+        Geniusfile(**incorrect_yaml_data)
+
+
+def test_missing_fields_validation():
+    """Test that the validation correctly identifies missing required fields."""
+    with pytest.raises(ValidationError):
+        Geniusfile(**missing_fields_data)
+
+
+def test_invalid_values_validation():
+    """Test that the validation correctly identifies invalid values for fields."""
+    with pytest.raises(ValidationError):
+        Geniusfile(**invalid_values_data)
+
+
+def test_extra_fields_validation():
+    """Test that the validation correctly handles extra fields."""
+    geniusfile = Geniusfile(**extra_fields_data)
+    assert geniusfile.spouts["github-batch"].args.extra_field == "extra_value"
+
+
+def test_different_state_types_validation():
+    """Test that the validation correctly identifies missing fields for different state types."""
+    data = correct_yaml_data.copy()
+    data["spouts"]["github-batch"]["state"]["type"] = "redis"
+
+    with pytest.raises(ValidationError):
+        Geniusfile(**data)
