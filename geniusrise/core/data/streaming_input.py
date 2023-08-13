d591c93095f1c892505d0e5ca550994b81f9bad7
ixaxaar <root@ixaxaar.in>
2023-08-13 15:36:52 +0530

    add docs to data

--- geniusrise/core/data/streaming_input.py
+++ geniusrise/core/data/streaming_input.py
@@ -15,25 +15,39 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-from typing import Callable
+from typing import Any, Callable, Iterator
 
 from kafka import KafkaConsumer
 
 from .input import InputConfig
 
-log = logging.getLogger(__name__)
-
 
 class StreamingInputConfig(InputConfig):
     """
-    Class for managing streaming input configurations.
+    📡 **StreamingInputConfig**: Manages streaming input configurations.
 
     Attributes:
         input_topic (str): Kafka topic to consume data.
         consumer (KafkaConsumer): Kafka consumer for consuming data.
+
+    Usage:
+    ```python
+    config = StreamingInputConfig("my_topic", "localhost:9092")
+    for message in config.iterator():
+        print(message.value)
+    ```
+
+    Note:
+    - Ensure the Kafka cluster is running and accessible.
+    - Adjust the `group_id` if needed.
     """
 
-    def __init__(self, input_topic: str, kafka_cluster_connection_string: str, group_id: str = "geniusrise"):
+    def __init__(
+        self,
+        input_topic: str,
+        kafka_cluster_connection_string: str,
+        group_id: str = "geniusrise",
+    ) -> None:
         """
         Initialize a new streaming input configuration.
 
@@ -43,61 +57,71 @@ class StreamingInputConfig(InputConfig):
             group_id (str, optional): Kafka consumer group id. Defaults to "geniusrise".
         """
         self.input_topic = input_topic
+        self.log = logging.getLogger(self.__class__.__name__)
         try:
             self.consumer = KafkaConsumer(
-                self.input_topic, bootstrap_servers=kafka_cluster_connection_string, group_id=group_id
+                self.input_topic,
+                bootstrap_servers=kafka_cluster_connection_string,
+                group_id=group_id,
             )
         except Exception as e:
-            log.exception(f"Failed to create Kafka consumer: {e}")
+            self.log.exception(f"🚫 Failed to create Kafka consumer: {e}")
             raise
             self.consumer = None
 
-    def get(self):
+    def get(self) -> KafkaConsumer:
         """
-        Get data from the input topic.
+        📥 Get data from the input topic.
 
         Returns:
             KafkaConsumer: The Kafka consumer.
+
+        Raises:
+            Exception: If no input source or consumer is specified.
         """
         if self.input_topic and self.consumer:
             try:
                 return self.consumer
             except Exception as e:
-                log.exception(f"Failed to consume from Kafka topic {self.input_topic}: {e}")
+                self.log.exception(f"🚫 Failed to consume from Kafka topic {self.input_topic}: {e}")
                 raise
-                return None
         else:
-            log.exception("No input source specified.")
+            self.log.exception("🚫 No input source specified.")
             raise
-            return None
 
-    def iterator(self):
+    def iterator(self) -> Iterator:
         """
-        Iterator method for yielding data from the Kafka consumer.
+        🔄 Iterator method for yielding data from the Kafka consumer.
 
         Yields:
             Kafka message: The next message from the Kafka consumer.
+
+        Raises:
+            Exception: If no Kafka consumer is available.
         """
         if self.consumer:
             try:
                 for message in self.consumer:
                     yield message
             except Exception as e:
-                log.exception(f"Failed to iterate over Kafka consumer: {e}")
+                self.log.exception(f"🚫 Failed to iterate over Kafka consumer: {e}")
                 raise
         else:
-            log.exception("No Kafka consumer available.")
+            self.log.exception("🚫 No Kafka consumer available.")
             raise
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator:
         """
         Make the class iterable.
         """
         return self
 
-    def __next__(self):
+    def __next__(self) -> Any:
         """
         Get the next message from the Kafka consumer.
+
+        Raises:
+            Exception: If no Kafka consumer is available or an error occurs.
         """
         if self.consumer:
             try:
@@ -105,56 +129,70 @@ class StreamingInputConfig(InputConfig):
             except StopIteration:
                 raise
             except Exception as e:
-                log.exception(f"Failed to get next message from Kafka consumer: {e}")
+                self.log.exception(f"🚫 Failed to get next message from Kafka consumer: {e}")
                 raise
-                return None
         else:
-            log.exception("No Kafka consumer available.")
+            self.log.exception("🚫 No Kafka consumer available.")
             raise
-            return None
 
-    def close(self):
+    def close(self) -> None:
         """
-        Close the Kafka consumer.
+        🚪 Close the Kafka consumer.
+
+        Raises:
+            Exception: If an error occurs while closing the consumer.
         """
         if self.consumer:
             try:
                 self.consumer.close()
             except Exception as e:
-                log.exception(f"Failed to close Kafka consumer: {e}")
+                self.log.exception(f"🚫 Failed to close Kafka consumer: {e}")
                 raise
 
-    def seek(self, partition: int, offset: int):
+    def seek(self, partition: int, offset: int) -> None:
         """
-        Change the position from which the Kafka consumer reads.
+        🔍 Change the position from which the Kafka consumer reads.
+
+        Args:
+            partition (int): The partition to seek.
+            offset (int): The offset to seek to.
+
+        Raises:
+            Exception: If an error occurs while seeking.
         """
         if self.consumer:
             try:
                 self.consumer.seek(partition, offset)
             except Exception as e:
-                log.exception(f"Failed to seek Kafka consumer: {e}")
+                self.log.exception(f"🚫 Failed to seek Kafka consumer: {e}")
                 raise
 
-    def commit(self):
+    def commit(self) -> None:
         """
-        Manually commit offsets.
+        ✅ Manually commit offsets.
+
+        Raises:
+            Exception: If an error occurs while committing offsets.
         """
         if self.consumer:
             try:
                 self.consumer.commit()
             except Exception as e:
-                log.exception(f"Failed to commit offsets: {e}")
+                self.log.exception(f"🚫 Failed to commit offsets: {e}")
                 raise
 
-    def filter_messages(self, filter_func: Callable):
+    def filter_messages(self, filter_func: Callable) -> Iterator:
         """
-        Filter messages from the Kafka consumer based on a filter function.
+        🔍 Filter messages from the Kafka consumer based on a filter function.
 
         Args:
             filter_func (callable): A function that takes a Kafka message and returns a boolean.
 
         Yields:
             Kafka message: The next message from the Kafka consumer that passes the filter.
+
+        Raises:
+            Exception: If no Kafka consumer is available or an error occurs.
         """
         if self.consumer:
             try:
@@ -162,8 +200,8 @@ class StreamingInputConfig(InputConfig):
                     if filter_func(message):
                         yield message
             except Exception as e:
-                log.exception(f"Failed to filter messages from Kafka consumer: {e}")
+                self.log.exception(f"🚫 Failed to filter messages from Kafka consumer: {e}")
                 raise
         else:
-            log.exception("No Kafka consumer available.")
+            self.log.exception("🚫 No Kafka consumer available.")
             raise
--- geniusrise/core/data/streaming_output.py
+++ geniusrise/core/data/streaming_output.py
@@ -13,28 +13,35 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 import json
 import logging
-from typing import Any
+from typing import Any, List
 
 from kafka import KafkaProducer
 
 from .output import OutputConfig
 
-log = logging.getLogger(__name__)
-
 
 class StreamingOutputConfig(OutputConfig):
     """
-    Class for managing streaming output configurations.
+    📡 **StreamingOutputConfig**: Manages streaming output configurations.
 
     Attributes:
         output_topic (str): Kafka topic to ingest data.
         producer (KafkaProducer): Kafka producer for ingesting data.
+
+    Usage:
+    ```python
+    config = StreamingOutputConfig("my_topic", "localhost:9092")
+    config.save({"key": "value"}, "ignored_filename")
+    config.flush()
+    ```
+
+    Note:
+    - Ensure the Kafka cluster is running and accessible.
     """
 
-    def __init__(self, output_topic: str, kafka_servers: str):
+    def __init__(self, output_topic: str, kafka_servers: str) -> None:
         """
         Initialize a new streaming output configuration.
 
@@ -43,49 +50,59 @@ class StreamingOutputConfig(OutputConfig):
             kafka_servers (str): Kafka bootstrap servers.
         """
         self.output_topic = output_topic
+        self.log = logging.getLogger(self.__class__.__name__)
         try:
             self.producer = KafkaProducer(bootstrap_servers=kafka_servers)
         except Exception as e:
-            log.exception(f"Failed to create Kafka producer: {e}")
+            self.log.exception(f"🚫 Failed to create Kafka producer: {e}")
             raise
             self.producer = None
 
-    def save(self, data: Any, filename: str):
+    def save(self, data: Any, filename: str) -> None:
         """
-        Ingest data into the Kafka topic.
+        📤 Ingest data into the Kafka topic.
 
         Args:
             data (Any): The data to ingest.
             filename (str): This argument is ignored for streaming outputs.
+
+        Raises:
+            Exception: If no Kafka producer is available or an error occurs.
         """
         if self.producer:
             try:
                 self.producer.send(self.output_topic, bytes(json.dumps(data).encode("utf-8")))
-                log.debug(f"Inserted the data into {self.output_topic} topic.")
+                self.log.debug(f"✅ Inserted the data into {self.output_topic} topic.")
             except Exception as e:
-                log.exception(f"Failed to send data to Kafka topic: {e}")
+                self.log.exception(f"🚫 Failed to send data to Kafka topic: {e}")
                 raise
         else:
-            log.exception("No Kafka producer available.")
+            self.log.exception("🚫 No Kafka producer available.")
             raise
 
-    def flush(self):
+    def flush(self) -> None:
         """
-        Flush the output by flushing the Kafka producer.
+        🔄 Flush the output by flushing the Kafka producer.
+
+        Raises:
+            Exception: If no Kafka producer is available.
         """
         if self.producer:
             self.producer.flush()
         else:
-            log.exception("No Kafka producer available.")
+            self.log.exception("🚫 No Kafka producer available.")
             raise
 
-    def send_key_value(self, key: Any, value: Any):
+    def send_key_value(self, key: Any, value: Any) -> None:
         """
-        Send a message with a key to the Kafka topic.
+        🔑 Send a message with a key to the Kafka topic.
 
         Args:
             key (Any): The key of the message.
             value (Any): The value of the message.
+
+        Raises:
+            Exception: If no Kafka producer is available or an error occurs.
         """
         if self.producer:
             try:
@@ -94,78 +111,92 @@ class StreamingOutputConfig(OutputConfig):
                     key=bytes(json.dumps(key).encode("utf-8")),
                     value=bytes(json.dumps(value).encode("utf-8")),
                 )
-                log.debug(f"Inserted the key-value pair into {self.output_topic} topic.")
+                self.log.debug(f"✅ Inserted the key-value pair into {self.output_topic} topic.")
             except Exception as e:
-                log.exception(f"Failed to send key-value pair to Kafka topic: {e}")
+                self.log.exception(f"🚫 Failed to send key-value pair to Kafka topic: {e}")
                 raise
         else:
-            log.exception("No Kafka producer available.")
+            self.log.exception("🚫 No Kafka producer available.")
             raise
 
-    def close(self):
+    def close(self) -> None:
         """
-        Close the Kafka producer.
+        🚪 Close the Kafka producer.
+
+        Raises:
+            Exception: If no Kafka producer is available.
         """
         if self.producer:
             self.producer.close()
             self.producer = None
         else:
-            log.exception("No Kafka producer available.")
+            self.log.exception("🚫 No Kafka producer available.")
             raise
 
-    def partition_available(self, partition: int):
+    def partition_available(self, partition: int) -> bool:
         """
-        Check if a partition is available in the Kafka topic.
+        🧐 Check if a partition is available in the Kafka topic.
 
         Args:
             partition (int): The partition to check.
 
         Returns:
             bool: True if the partition is available, False otherwise.
+
+        Raises:
+            Exception: If no Kafka producer is available.
         """
         if self.producer:
             return partition in self.producer.partitions_for(self.output_topic)
         else:
-            log.exception("No Kafka producer available.")
+            self.log.exception("🚫 No Kafka producer available.")
             raise
             return False
 
-    def save_to_partition(self, value: Any, partition: int):
+    def save_to_partition(self, value: Any, partition: int) -> None:
         """
-        Send a message to a specific partition in the Kafka topic.
+        🎯 Send a message to a specific partition in the Kafka topic.
 
         Args:
             value (Any): The value of the message.
             partition (int): The partition to send the message to.
+
+        Raises:
+            Exception: If no Kafka producer is available or an error occurs.
         """
         if self.producer:
             try:
                 self.producer.send(
-                    self.output_topic, value=bytes(json.dumps(value).encode("utf-8")), partition=partition
+                    self.output_topic,
+                    value=bytes(json.dumps(value).encode("utf-8")),
+                    partition=partition,
                 )
-                log.debug(f"Inserted the message into partition {partition} of {self.output_topic} topic.")
+                self.log.debug(f"✅ Inserted the message into partition {partition} of {self.output_topic} topic.")
             except Exception as e:
-                log.exception(f"Failed to send message to Kafka topic: {e}")
+                self.log.exception(f"🚫 Failed to send message to Kafka topic: {e}")
                 raise
         else:
-            log.exception("No Kafka producer available.")
+            self.log.exception("🚫 No Kafka producer available.")
             raise
 
-    def save_bulk(self, messages: list):
+    def save_bulk(self, messages: List[Any]) -> None:
         """
-        Send multiple messages at once to the Kafka topic.
+        📦 Send multiple messages at once to the Kafka topic.
 
         Args:
             messages (list): The messages to send.
+
+        Raises:
+            Exception: If no Kafka producer is available or an error occurs.
         """
         if self.producer:
             try:
                 for message in messages:
                     self.producer.send(self.output_topic, bytes(json.dumps(message).encode("utf-8")))
-                log.debug(f"Inserted {len(messages)} messages into {self.output_topic} topic.")
+                self.log.debug(f"✅ Inserted {len(messages)} messages into {self.output_topic} topic.")
             except Exception as e:
-                log.exception(f"Failed to send messages to Kafka topic: {e}")
+                self.log.exception(f"🚫 Failed to send messages to Kafka topic: {e}")
                 raise
         else:
-            log.exception("No Kafka producer available.")
+            self.log.exception("🚫 No Kafka producer available.")
             raise
