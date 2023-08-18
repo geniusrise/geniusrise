0d9c9dfac961b7b540f8913ddd16111173993ade
ixaxaar <root@ixaxaar.in>
2023-08-19 00:48:55 +0530

    publish to pypi

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 
-install:
+setup:
 	@pip install -r ./requirements.txt
 
-developer-install:
+developer-setup:
 	@pip install -r ./requirements.txt
 	@pip install -r ./requirements-dev.txt
 
 test:
 	@coverage run -m pytest -v ./tests
+
+install:
+	@~/.local/bin/pip install . --user --break-system-packages
+
+publish:
+	@python setup.py sdist bdist_wheel
+	@twine upload dist/geniusrise-${GENIUSRISE_VERSION}-* --verbose
---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+entry_points = {
+    "console_scripts": ["geniusrise = geniusrise.cli.geniusctl:main", "genius = geniusrise.cli.geniusctl:main"]
+}
+
+setup(
+    name="geniusrise",
+    version=os.environ.get("GENIUSRISE_VERSION" "0.1.0"),
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    install_requires=requirements,
+    entry_points=entry_points,
+    python_requires=">=3.10",
+    author="Geniusrise",
+    author_email="ixaxaar@geniusrise.ai",
+    description="An MLOps framework for LLMs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/geniusrise/geniusrise",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    keywords="mlops, llm, geniusrise, machine learning, data processing",
+    project_urls={
+        "Bug Reports": "https://github.com/geniusrise/geniusrise/issues",
+        "Source": "https://github.com/geniusrise/geniusrise",
+        "Documentation": "https://docs.geniusrise.ai/",
+    },
+    package_data={
+        "geniusrise": ["path/to/additional/data/files/*"],
+    },
+    extras_require={
+        "dev": ["check-manifest"],
+        "test": ["coverage"],
+    },
+)
