68aa00ccd5117ec00503e633876545793a4bdde0
ixaxaar <root@ixaxaar.in>
2023-09-17 13:18:01 +0530

    bummp vesion

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup(
     name="geniusrise",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=requirements,
     entry_points=entry_points,
