38a19bf434146d2d2326c00db00c93174d2a7a32
ixaxaar <root@ixaxaar.in>
2023-09-25 02:27:20 +0530

    rmeove unnecessary stuff, make methods abstract

--- geniusrise/core/data/input.py
+++ geniusrise/core/data/input.py
@@ -15,9 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
-import time
 from abc import ABC, abstractmethod
-from contextlib import contextmanager
 from typing import Any, Dict, Union
 
 from retrying import retry
@@ -29,14 +27,10 @@ class Input(ABC):
 
     Attributes:
         log (logging.Logger): Logger instance.
-        start_time (float): Start time for metrics.
-        end_time (float): End time for metrics.
     """
 
     def __init__(self) -> None:
         self.log = logging.getLogger(self.__class__.__name__)
-        self.start_time: float = -1.0
-        self.end_time: float = -1.0
 
     @abstractmethod
     def get(self) -> Any:
@@ -46,8 +40,8 @@ class Input(ABC):
         Returns:
             Any: The data from the input source.
         """
-        pass
 
+    @abstractmethod
     def collect_metrics(self) -> Dict[str, float]:
         """
         Collect metrics like latency.
@@ -55,24 +49,6 @@ class Input(ABC):
         Returns:
             Dict[str, float]: A dictionary containing metrics.
         """
-        self.end_time = time.time()
-        latency = self.end_time - self.start_time
-        return {"latency": latency}
-
-    def validate_data(self, data: Any) -> bool:
-        """
-        Validate the incoming data.
-
-        Args:
-            data (Any): The data to validate.
-
-        Returns:
-            bool: True if data is valid, False otherwise.
-        """
-        if not data:
-            self.log.error("Data is empty.")
-            return False
-        return True
 
     @retry(stop_max_attempt_number=3, wait_fixed=2000)
     def retryable_get(self) -> Any:
@@ -84,20 +60,7 @@ class Input(ABC):
         """
         return self.get()
 
-    @contextmanager
-    def timeout(self, seconds: int):
-        """
-        Timeout context manager.
-
-        Args:
-            seconds (int): Timeout in seconds.
-        """
-        start = time.time()
-        yield
-        end = time.time()
-        if end - start > seconds:
-            raise TimeoutError("Operation timed out.")
-
+    @abstractmethod
     def compose(self, *inputs: "Input") -> Union[bool, str]:
         """
         Compose multiple inputs.
@@ -108,12 +71,6 @@ class Input(ABC):
         Returns:
             Union[bool, str]: True if successful, error message otherwise.
         """
-        try:
-            # Placeholder for composing logic
-            return True
-        except Exception as e:
-            self.log.error(f"Failed to compose inputs: {e}")
-            return str(e)
 
     def __add__(self, *inputs: "Input") -> Union[bool, str]:
         """
