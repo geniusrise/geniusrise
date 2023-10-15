e472afb3e441e0dcbb89120f87c84447e2f02a4e
ixaxaar <root@ixaxaar.in>
2023-10-15 09:13:47 +0530

    change test names

--- geniusrise/core/tests/data/test_batch_input.py
+++ geniusrise/core/tests/data/test_batch_input.py
@@ -30,109 +30,109 @@ spark = SparkSession.builder.master("local[1]").appName("GeniusRise").getOrCreat
 
 # Define a fixture for BatchInput
 @pytest.fixture
-def batch_input_config(tmpdir):
+def batch_input(tmpdir):
     yield BatchInput(str(tmpdir), BUCKET, S3_FOLDER)
 
 
 # Test that the BatchInput can be initialized
-def test_batch_input_config_init(batch_input_config):
-    assert batch_input_config.input_folder is not None
-    assert batch_input_config.bucket == BUCKET
-    assert batch_input_config.s3_folder == S3_FOLDER
+def test_batch_input_init(batch_input):
+    assert batch_input.input_folder is not None
+    assert batch_input.bucket == BUCKET
+    assert batch_input.s3_folder == S3_FOLDER
 
 
 # Test that the BatchInput can get the input folder
-def test_batch_input_config_get(batch_input_config):
-    assert batch_input_config.get() == batch_input_config.input_folder
+def test_batch_input_get(batch_input):
+    assert batch_input.get() == batch_input.input_folder
 
 
-def test_batch_input_config_validate_file(batch_input_config):
+def test_batch_input_validate_file(batch_input):
     test_file = "test_file.txt"
-    with open(os.path.join(batch_input_config.input_folder, test_file), "w") as f:
+    with open(os.path.join(batch_input.input_folder, test_file), "w") as f:
         f.write("test")
-    assert batch_input_config._validate_file(test_file)  # Method is now private
-    assert not batch_input_config._validate_file("nonexistent.txt")
+    assert batch_input._validate_file(test_file)  # Method is now private
+    assert not batch_input._validate_file("nonexistent.txt")
 
 
 # Test that the BatchInput can list files in the input folder
-def test_batch_input_config_list_files(batch_input_config):
+def test_batch_input_list_files(batch_input):
     test_file = "test_file.txt"
-    with open(os.path.join(batch_input_config.input_folder, test_file), "w") as f:
+    with open(os.path.join(batch_input.input_folder, test_file), "w") as f:
         f.write("test")
-    files = list(batch_input_config.list_files())
+    files = list(batch_input.list_files())
     assert len(files) == 1
     assert test_file in files[0]
 
 
 # Test that the BatchInput can read a file from the input folder
-def test_batch_input_config_read_file(batch_input_config):
+def test_batch_input_read_file(batch_input):
     test_file = "test_file.txt"
-    with open(os.path.join(batch_input_config.input_folder, test_file), "w") as f:
+    with open(os.path.join(batch_input.input_folder, test_file), "w") as f:
         f.write("test")
-    contents = batch_input_config.read_file(test_file)
+    contents = batch_input.read_file(test_file)
     assert contents == "test"
 
 
 # Test that the BatchInput can delete a file from the input folder
-def test_batch_input_config_delete_file(batch_input_config):
+def test_batch_input_delete_file(batch_input):
     test_file = "test_file.txt"
-    with open(os.path.join(batch_input_config.input_folder, test_file), "w") as f:
+    with open(os.path.join(batch_input.input_folder, test_file), "w") as f:
         f.write("test")
-    batch_input_config.delete_file(test_file)
-    assert not os.path.exists(os.path.join(batch_input_config.input_folder, test_file))
+    batch_input.delete_file(test_file)
+    assert not os.path.exists(os.path.join(batch_input.input_folder, test_file))
 
 
 # Test Spark DataFrame creation
-def test_batch_input_config_spark_df(batch_input_config):
+def test_batch_input_spark_df(batch_input):
     test_file = "test_spark.txt"
-    with open(os.path.join(batch_input_config.input_folder, test_file), "w") as f:
+    with open(os.path.join(batch_input.input_folder, test_file), "w") as f:
         f.write("spark test")
-    df = batch_input_config.spark_df(spark)
+    df = batch_input.spark_df(spark)
     assert df.count() == 1
     assert df.first().content == "spark test"
 
 
 # Test composing multiple BatchInput instances
-def test_batch_input_config_compose(batch_input_config, tmpdir):
+def test_batch_input_compose(batch_input, tmpdir):
     another_input = BatchInput(str(tmpdir.mkdir("another")), BUCKET, S3_FOLDER)
     test_file = "test_compose.txt"
     with open(os.path.join(another_input.input_folder, test_file), "w") as f:
         f.write("compose test")
-    assert batch_input_config.compose(another_input)
-    assert os.path.exists(os.path.join(batch_input_config.input_folder, test_file))
+    assert batch_input.compose(another_input)
+    assert os.path.exists(os.path.join(batch_input.input_folder, test_file))
 
 
 # Test collecting metrics
-def test_batch_input_config_collect_metrics(batch_input_config):
-    metrics = batch_input_config.collect_metrics()
+def test_batch_input_collect_metrics(batch_input):
+    metrics = batch_input.collect_metrics()
     assert isinstance(metrics, dict)
 
 
 # Test _get_partitioned_key
-def test_batch_input_config_get_partitioned_key(batch_input_config):
-    batch_input_config.partition_scheme = "%Y/%m/%d"
-    partitioned_key = batch_input_config._get_partitioned_key(S3_FOLDER)
+def test_batch_input_get_partitioned_key(batch_input):
+    batch_input.partition_scheme = "%Y/%m/%d"
+    partitioned_key = batch_input._get_partitioned_key(S3_FOLDER)
     assert partitioned_key.startswith(S3_FOLDER)
 
 
 # Test FileNotExistError in spark_df
-def test_batch_input_config_spark_df_error():
+def test_batch_input_spark_df_error():
     with pytest.raises(FileNotExistError):
         batch_input = BatchInput("nonexistent_folder", BUCKET, S3_FOLDER)
         batch_input.spark_df(spark)
 
 
 # Test that the BatchInput can copy files from the S3 bucket
-def test_batch_input_config_copy_from_remote(batch_input_config):
+def test_batch_input_copy_from_remote(batch_input):
     # Upload a test file to the S3 bucket
     s3 = boto3.client("s3")
     s3.put_object(Body="test content", Bucket=BUCKET, Key=f"{S3_FOLDER}/test_file_from_s3.txt")
 
     # Run the copy_from_remote method
-    batch_input_config.copy_from_remote()
+    batch_input.copy_from_remote()
 
     # Check that the files were copied to the input folder
-    copied_files = os.listdir(os.path.join(batch_input_config.input_folder, S3_FOLDER))
+    copied_files = os.listdir(os.path.join(batch_input.input_folder, S3_FOLDER))
     assert "test_file_from_s3.txt" in copied_files
 
     # Clean up the test file from the S3 bucket
