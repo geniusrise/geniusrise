cf47f2801eccf6c93268a079b3039663af1435a0
ixaxaar <root@ixaxaar.in>
2024-02-19 16:15:15 +0530

    upgrade numpy

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup(
     name="geniusrise",
-    version="0.0.33",
+    version="0.0.35",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=requirements,
     entry_points=entry_points,
