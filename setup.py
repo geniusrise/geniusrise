08f3e6735e4452f1c636bc81892402bccf44a6c9
ixaxaar <root@ixaxaar.in>
2024-02-20 01:12:23 +0530

    fix psycopg2 compilation requirement

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup(
     name="geniusrise",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=requirements,
     entry_points=entry_points,
