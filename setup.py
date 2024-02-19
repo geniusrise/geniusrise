d1e74b883bc42d8f60db5820091e2edf144575d4
ixaxaar <root@ixaxaar.in>
2024-02-19 17:33:12 +0530

    remove flink

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup(
     name="geniusrise",
-    version="0.0.36",
+    version="0.0.39",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=requirements,
     entry_points=entry_points,
