8a5a1d2f381492929e06c2b1663a1b4ab6228ba8
ixaxaar <root@ixaxaar.in>
2024-05-10 12:24:14 +0530

    change license header

--- geniusrise/runners/k8s/base.py
+++ geniusrise/runners/k8s/base.py
@@ -1,18 +1,17 @@
 # 🧠 Geniusrise
 # Copyright (C) 2023  geniusrise.ai
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+#  http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import base64
 import json
@@ -269,9 +268,9 @@ class K8sResourceManager:
                     image_pull_policy="Always",
                 )
             ],
-            image_pull_secrets=[client.V1LocalObjectReference(name=image_pull_secret_name)]
-            if image_pull_secret_name
-            else None,
+            image_pull_secrets=(
+                [client.V1LocalObjectReference(name=image_pull_secret_name)] if image_pull_secret_name else None
+            ),
         )
 
     def __wait_for_pod_completion(self, pod_name: str, timeout: int = 600, poll_interval: int = 5) -> bool:
